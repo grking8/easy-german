17cfef8801588d50700033aaa416e71df371ec0f
family-guy <grking8@gmail.com>
2017-10-08 20:52:48 +0000

    Refactor

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ CHANNEL_ID = 'UCbxb2fqe9oNgglAoYqsYOtQ'
 MIME_TYPE = 'audio/mp3'
 EXTENSION = 'mp3'
 MAX_RESULTS = 6
+API_BASE_URL = 'https://www.googleapis.com/youtube/v3/'
+VIDEO_BASE_URL = 'https://www.youtube.com/watch?v={}'
+YOUTUBE_KEY = os.environ['YOUTUBE_KEY']
 
 
 def clear_local_media(tmp_dir):
@@ -32,19 +35,17 @@ def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
-    api_base_url = 'https://www.googleapis.com/youtube/v3/'
     r = requests.get('{}channels?part=contentDetails&id={}&key={}'.format(
-        api_base_url, CHANNEL_ID, os.environ['YOUTUBE_KEY']))
+        API_BASE_URL, CHANNEL_ID, YOUTUBE_KEY))
     playlist_id = r.json()['items'][0]['contentDetails']['relatedPlaylists']['uploads']  # noqa
     s = requests.get(
         '{}playlistItems?part=snippet&maxResults={}&playlistId={}&key={}'
-        .format(api_base_url, MAX_RESULTS, playlist_id,
-                os.environ['YOUTUBE_KEY']))
-    video_base_url = 'https://www.youtube.com/watch?v={}'
+        .format(API_BASE_URL, MAX_RESULTS, playlist_id,
+                YOUTUBE_KEY))
 
     for item in s.json()['items']:
         video_id = item['snippet']['resourceId']['videoId']
-        video_url = video_base_url.format(video_id)
+        video_url = VIDEO_BASE_URL.format(video_id)
         file_name = '{}.{}'.format(video_id, EXTENSION)
         tmp_dir = tempfile.mkdtemp()
         atexit.register(clear_local_media, tmp_dir=tmp_dir)
