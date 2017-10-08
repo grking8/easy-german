e68d659fda52a9a087ca6390996fb87df693b512
family-guy <grking8@gmail.com>
2017-10-08 18:41:19 +0000

    Loop over video item results

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import requests
 CHANNEL_ID = 'UCbxb2fqe9oNgglAoYqsYOtQ'
 MIME_TYPE = 'audio/mp3'
 EXTENSION = 'mp3'
-MAX_RESULTS = 20
+MAX_RESULTS = 2
 ORDER = 'date'
 PART = 'snippet,id'
 
@@ -28,14 +28,15 @@ def main():
         os.environ['YOUTUBE_KEY'], CHANNEL_ID, PART, ORDER, MAX_RESULTS)
     search_url = '{}?{}'.format(search_url, qs)
     items = requests.get(search_url).json()['items']
-    base_url = 'https://www.youtube.com/watch?v={}'
-    video_id = items[0]['id']['videoId']
-    video_url = base_url.format(video_id)
-    subprocess.check_output(['youtube-dl', '--extract-audio', '--audio-format',
-                             'mp3', '--id', '--newline', '--verbose',
-                             video_url])
-    path = '{}.{}'.format(video_id, EXTENSION)
-    upload_media(service, path, MIME_TYPE)
+    video_url = 'https://www.youtube.com/watch?v={}'
+
+    for item in items:
+        video_id = item['id']['videoId']
+        subprocess.check_output(['youtube-dl', '--extract-audio',
+                                 '--audio-format', 'mp3', '--id', '--newline',
+                                 '--verbose', video_url.format(video_id)])
+        path = '{}.{}'.format(video_id, EXTENSION)
+        upload_media(service, path, MIME_TYPE)
 
 
 if __name__ == '__main__':
