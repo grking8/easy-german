f6bb21c056caebc44c223ff293b114857c0bd9e1
family-guy <grking8@gmail.com>
2017-11-05 21:49:38 +0000

    Linting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         'requests==2.18.1',
         'google-api-python-client==1.6.2',
         'youtube-dl==2017.6.18',
-	'pytest==3.2.3',
+        'pytest==3.2.3',
     ],
     url='https://github.com/family-guy/easy-german',
     author='Guy King',
